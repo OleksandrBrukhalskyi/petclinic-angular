2f5c8de7e0a64ab52e266245e86a3201d4d9373c
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-04-17 17:37:05 +0300

    feat: create Date instance in Pet constructor.

--- src/app/model/pet.ts
+++ src/app/model/pet.ts
@@ -1,12 +1,13 @@
 export class Pet {
 	id: any;
 	name: string;
-	date_of_birth: Date = new Date();
+	date_of_birth: any;
 	breed: string;
 	owner: any;
 
 	constructor(){
 		this.owner = new Owner();
+		this.date_of_birth = new Date();
 	}
 
 }
