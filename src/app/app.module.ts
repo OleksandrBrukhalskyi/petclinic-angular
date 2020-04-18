7acf427a724953dd01facdf6a3fc7c2f24a113ac
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-04-18 18:18:50 +0300

    feat: add Material UI modules in app.module.ts.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,39 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { OwnerComponent } from './owner/owner.component';
+import { HttpClientModule } from '@angular/common/http';
+import {MatTableModule} from "@angular/material/table";
+import {CdkTableModule} from "@angular/cdk/table";
+import {MatSortModule} from "@angular/material/sort";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatProgressBarModule} from "@angular/material/progress-bar";
+import {MatButtonModule} from "@angular/material/button";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OwnerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatTableModule,
+    CdkTableModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    MatButtonModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
