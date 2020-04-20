84ebd4bfef9db73b1fbf9aa9b40c3ec76ec8b8d9
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-04-20 15:54:25 +0300

    feat: add action column in displayedColumns.

--- src/app/owner/owner.component.ts
+++ src/app/owner/owner.component.ts
@@ -3,6 +3,9 @@ import {OwnerService} from '../service/owner.service';
 import {Owner} from '../model/owner';
 import {MatTableDataSource} from '@angular/material/table';
 import {MatSort} from '@angular/material/sort';
+import {NgForm} from "@angular/forms";
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {MatPaginator} from "@angular/material/paginator";
 
 
 @Component({
@@ -12,7 +15,7 @@ import {MatSort} from '@angular/material/sort';
 })
 export class OwnerComponent implements OnInit {
 
-  displayedColumns = ['id', 'surname', 'firstname', 'patronymic', 'address', 'phone_number' ];
+  displayedColumns = ['id', 'surname', 'firstname', 'patronymic', 'address', 'phone_number' , 'actions' ];
   @ViewChild(MatSort, {static: true}) sort: MatSort;
 
 	owners: any;
@@ -29,8 +32,7 @@ export class OwnerComponent implements OnInit {
     this.dataSource = new MatTableDataSource();
     this.load();
     this.dataSource.sort = this.sort;
-
-
+    this.dataSource.paginator = this.paginator;
   }
 
   load() {
@@ -46,7 +48,7 @@ export class OwnerComponent implements OnInit {
     });
   }
 
-  create(){
+  create(f: NgForm){
     this.ownerService.add(this.owner).subscribe(() => {
       this.load();
     });
@@ -57,7 +59,7 @@ export class OwnerComponent implements OnInit {
   }
   save() {
     if(this.ownerToUpdate.id !== '' && this.ownerToUpdate.surname !== '' && this.ownerToUpdate.firstname !== ''
-    && this.ownerToUpdate.patronymic !== '' && this.ownerToUpdate.address !== '' && this.ownerToUpdate.phoneNumber !== '') {
+    && this.ownerToUpdate.patronymic !== '' && this.ownerToUpdate.homeAddress !== '' && this.ownerToUpdate.phoneNumber !== '') {
       this.ownerService.update(this.ownerToUpdate , this.id).subscribe(() => {
         this.load();
         this.ownerToUpdate = null;
@@ -65,7 +67,7 @@ export class OwnerComponent implements OnInit {
     }
   }
 
-  delete(id: any){
+  delete(id: any) {
     this.ownerService.delete(id).subscribe(() => {
       this.load();
     });
@@ -74,7 +76,6 @@ export class OwnerComponent implements OnInit {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
-
 
 
 }
