4cd3cac1d2612735bb03126e13f4fd4e3e3fe6e6
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-04-26 15:35:49 +0300

    implement methods in Pet service.

--- src/app/service/pet.service.ts
+++ src/app/service/pet.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Pet} from "../model/pet";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PetService {
+  baseUrl = `http://localhost:8080/api/pet`;
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getPets() {
+    return this.http.get(`${this.baseUrl}`);
+  }
+  add(pet: Pet) {
+    return this.http.post(`${this.baseUrl}/add`, pet);
+  }
+  update(pet: Pet, id: number) {
+    return this.http.put(`${this.baseUrl}/${id}`, pet);
+  }
+  delete(id: any) {
+    return this.http.delete(`${this.baseUrl}/${id}`);
+  }
+  getById(id: any) {
+    return this.http.get(`${this.baseUrl}/${id}`);
+  }
 }
