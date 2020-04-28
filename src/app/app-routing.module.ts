35059f2a73df11bf4c06e6dc24bf6e714f0a7bd9
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-04-28 20:57:26 +0300

    feat: add routing to the pet component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {OwnerComponent} from './owner/owner.component';
-import {PetComponent} from "./pet/pet.component";
+import {PetComponent} from './pet/pet.component';
 
 
 const routes: Routes = [
